b69c8ad6e118791569da8a9ef273b19c58e9bf40
aadar <aadar100@gmail.com>
2024-06-01 15:01:09 -0400

    Fix issues

--- back-end/app/db/models.py
+++ back-end/app/db/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
-from .db.database import Base
+from .database import Base
 
 class Submission(Base):
     # Save it under the table "submissions"
