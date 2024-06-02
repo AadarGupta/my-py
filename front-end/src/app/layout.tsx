e1942f1cc4423495e0ac8077ec09b3cef95dccb2
aadar <aadar100@gmail.com>
2024-06-01 22:49:03 -0400

    Rename project

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My Py",
-  description: "Python code editor",
+  title: "RunPy",
+  description: "Python 3.0 Compiler",
 };
 
 export default function RootLayout({
